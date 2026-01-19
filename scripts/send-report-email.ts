265abdb73db3bde8ceecfde1000c59287bd656f5
Clarke Moyer <clarkemoyer@freeforcharity.org>
2026-01-19 13:13:42 -0500

    chore: remove debug logging

--- scripts/send-report-email.ts
+++ scripts/send-report-email.ts
@@ -26,7 +26,6 @@ async function sendEmail() {
     const sessions = totals[2]?.value || '0'
     const views = totals[4]?.value || '0'
 
-    console.log('DEBUG: Configuring Gmail Transport...')
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
